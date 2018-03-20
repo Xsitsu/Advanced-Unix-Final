ecfd1e370c7de1edde815f0fe44dbf5b0d5d79e5
Xsitsu <jacoblocke21@live.com>
2018-03-20 14:41:30 -0700

    Got calculator stuff to work. You can input numbers and operators and it displays everything correctly. Just need to send it to the server and get a result back.

--- client/client.c
+++ client/client.c
@@ -9,8 +9,14 @@
 #define HEIGHT 20
 
 #define CKEY_ENTER 10
+#define CKEY_BACK 263
 #define CKEY_PLUS 43
 #define CKEY_MINUS 45
+#define CKEY_UP 259
+#define CKEY_DOWN 258
+
+#define OPCHAR_PLUS "+"
+#define OPCHAR_MINUS "-"
 
 int startx;
 int starty;
@@ -20,6 +26,12 @@ int mid_y;
 
 int num_x;
 int num_y;
+int op;
+
+int offset_x;
+int offset_y;
+
+bool write_x;
 
 void draw_calc(WINDOW *win);
 int send_math();
@@ -37,39 +49,106 @@ int main()
 	startx = (SCREEN_WIDTH - WIDTH) / 2;
 	starty = (SCREEN_HEIGHT - HEIGHT) / 2;
 
-	mid_x = SCREEN_WIDTH / 2;
-	mid_y = SCREEN_HEIGHT / 2;
+	mid_x = (WIDTH / 2);
+	mid_y = (HEIGHT / 2);
 
 
 	win = newwin(HEIGHT, WIDTH, starty, startx);
 	keypad(win, TRUE);
 	curs_set(0);
 
-	int x = 0;
-	int y = 0;
-	int op = 0;
+	offset_x = 1;
+	offset_y = 1;
+	num_x = 0;
+	num_y = 0;
+	op = CKEY_PLUS;
 
+	write_x = true;
 	bool playing = true;
 	while (playing)
 	{
 		draw_calc(win);
 		wrefresh(win);
 		key = wgetch(win);
-		if (key >= 48 && key < 57)
+		if (key >= 48 && key <= 57)
 		{
-			
+			int* num;
+			int* offset;
+			if (write_x)
+			{
+				num = &num_x;
+				offset = &offset_x;
+			}
+			else
+			{
+				num = &num_y;
+				offset = &offset_y;
+			}
+
+			int n = *num;
+			if (n == 0)
+			{
+				n = (key - 48);
+				*num = n;
+			}
+			else if (n < 1000)
+			{
+				n = (n * 10) + (key - 48);
+				*num = n;
+				*offset = (*offset + 1);
+			}
+		}
+		else if (key == CKEY_BACK)
+		{
+			int* num;
+			int* offset;
+			if (write_x)
+			{
+				num = &num_x;
+				offset = &offset_x;
+			}
+			else
+			{
+				num = &num_y;
+				offset = &offset_y;
+			}
+
+			int n = *num;
+			if (n > 10)
+			{
+				n = (n / 10);
+				*num = n;
+				*offset = (*offset - 1);
+			}
+			else if (n > 0)
+			{
+				n = (n / 10);
+				*num = n;
+			}
 		}
 		else if (key == CKEY_ENTER)
 		{
-			printf("KEY ENTER\n");
+			int result = send_math();
 		}
 		else if (key == CKEY_PLUS)
 		{
-			printf("KEY PLUS\n");
+			op = CKEY_PLUS;
 		}
 		else if (key == CKEY_MINUS)
 		{
-			printf("KEY MINUS\n");
+			op = CKEY_MINUS;
+		}
+		else if (key == CKEY_UP)
+		{
+			write_x = true;
+		}
+		else if (key == CKEY_DOWN)
+		{
+			write_x = false;
+		}
+		else
+		{
+			//printf("Key: %d\n", key);
 		}
 	}
 
@@ -82,9 +161,59 @@ void draw_calc(WINDOW *win)
 	int y = mid_y - 6;
 
 	wmove(win, y, x - (37 / 2));
-	wprintw(win, "Use the numpad to use the calculator!\n");
-	wmove(win, y + 1, x - (28 / 2));
-	wprintw(win, "Maximum 4 digits per number.\n");
+	wprintw(win, "Use the numpad to use the calculator!");
+	wmove(win, y + 1, x - (42 / 2));
+	wprintw(win, "Arrow keys up down to select input number!");
+	wmove(win, y + 2, x - (48 / 2));
+	wprintw(win, "Enter to send results to server for computation!");
+	wmove(win, y + 3, x - (28 / 2));
+	wprintw(win, "Maximum 4 digits per number.");
+
+	wmove(win, y + 5, x);
+	for (int i = 4; i > offset_x; i--)
+	{
+		wprintw(win, " ");
+	}
+	wprintw(win, "%d", num_x);
+
+	if (write_x)
+	{
+		wprintw(win, " <--");
+	}
+	else
+	{
+		wprintw(win, "    ");
+	}
+
+	wmove(win, y + 6, x - 2);
+	if (op == CKEY_PLUS)
+	{
+		wprintw(win, OPCHAR_PLUS);
+	}
+	else if (op == CKEY_MINUS)
+	{
+		wprintw(win, OPCHAR_MINUS);
+	}
+	else
+	{
+		wprintw(win, "?");
+	}
+
+	wmove(win, y + 6, x);
+	for (int i = 4; i > offset_y; i--)
+	{
+		wprintw(win, " ");
+	}
+	wprintw(win, "%d", num_y);
+
+	if (!write_x)
+	{
+		wprintw(win, " <--");
+	}
+	else
+	{
+		wprintw(win, "    ");
+	}
 }
 
 int send_math()
